d6ac6fe19649ff36bb1e0e7b6719745ed47e56a1
ashaimaliazhr <asha.imaliazahra2006@students.unila.ac.id>
2025-01-24 19:14:59 +0700

    update ui

--- app/Views/login.php
+++ app/Views/login.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in</title>
+  <title>Inventory Barang | Log in</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -18,7 +18,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href="../../index2.html"><b>Inventory</b>System</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
@@ -28,7 +28,7 @@
       <form action="<?= base_url('login') ?>" method="post">
       <?= csrf_field() ?> 
         <div class="input-group mb-3">
-          <input type="text" name="username" required>
+          <input type="text" class="form-control" placeholder="Username">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -60,22 +60,8 @@
         </div>
       </form>
 
-      <div class="social-auth-links text-center mb-3">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
-      <!-- /.social-auth-links -->
-
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
       <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
+        <a href="register.html" class="text-center">Register</a>
       </p>
     </div>
     <!-- /.login-card-body -->
